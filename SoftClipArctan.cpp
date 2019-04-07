8893e683bbbb62241727783a5ade79135f8a2fcf
BrettStory <brettmstory@gmail.com>
2019-04-07 08:32:16 -0500

    Feature: a standard hard clip and a soft clip that utilizes arctan have been added.

---
+++ SoftClipArctan.cpp
@@ -0,0 +1,15 @@
+#include "SoftClipArctan.hpp"
+#ifndef _USE_MATH_DEFINES
+#define _USE_MATH_DEFINES
+#endif // !_USE_MATH_DEFINES
+#include <cmath>
+
+namespace brettstory {
+	namespace audio {
+		namespace clipping {
+			double SoftClipArctan::ClipSample(double sample, double boost) {
+				return atan(sample * boost) * (M_PI_2);
+			}
+		}
+	}
+}
